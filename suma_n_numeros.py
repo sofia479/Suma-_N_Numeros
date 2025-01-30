42ac6a975f541870aa78d9a4a1e7790912b3985e
sofia479 <bertagalvis@colegioguanta.edu.co>
2025-01-30 17:35:28 -0500

    atualizacion

---
+++ suma_n_numeros.py
@@ -0,0 +1,14 @@
+# imput
+print("##########################")
+n= input("dijite su numero ") 
+n = int(n)
+print("#########################") 
+
+
+
+# formula
+s = (n*(n+1))/ 2
+
+#output
+print("###########################")
+print("la suma de  los numeros " + str (s))
